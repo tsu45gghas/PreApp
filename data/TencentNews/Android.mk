322295b20d8e01fae7699707b6574efbed152808
dingchao <daniel.dingchao@gmail.com>
2015-12-01 09:13:43 +0800

    add prebuild app

---
+++ data/TencentNews/Android.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2011-2013 The Android-x86 Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+# Module name should match apk name to be installed
+LOCAL_MODULE := TencentNews
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/vendor/helium/app
+
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
